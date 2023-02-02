af1b9d0611d9e38fc1a892714232dc162e5a5a5e
fatih ozdil <muhammedfatihozdil@gmail.com>
2023-02-02 03:20:10 +0300

    update production code

--- blog/settings.py
+++ blog/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
+mimetypes.add_type("text/html", ".html", True)
+mimetypes.add_type("text/html", ".js", True)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
