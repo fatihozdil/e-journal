c8e90628152d194e01007626badc0d64fe1aae2a
fatih ozdil <muhammedfatihozdil@gmail.com>
2023-02-02 16:14:53 +0300

    update production codes

--- blog/production.py
+++ blog/production.py
@@ -1,11 +1,11 @@
 import os
-from   blog.settings.development import *
+from   blog.development import *
 
 # Configure the domain name using the environment variable
 # that Azure automatically creates for us.
+DEBUG = False
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
-DEBUG = False
 
 # WhiteNoise configuration
 MIDDLEWARE = [                                                                   
